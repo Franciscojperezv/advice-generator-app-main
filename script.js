ce134efdff66dd90204242ed9a43b5144f5bda91
Francisco Pérez <fjavielpv@gmail.com>
2022-05-09 13:42:52 -0400

    Fetching working, challenge complete.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function newAdvice(){
-    document.getElementById("main_advice").innerText = "New advice";
+"strict mode";
+const numAdvice = document.querySelector('#main_advice-number');
+const txtAdvice = document.querySelector('#main_advice_text');
+const myAPI = "https://api.adviceslip.com/advice";
+
+getAdvice();
+
+function getAdvice(){
+  fetch(myAPI)
+    .then(respuesta => respuesta.json())
+    .then(respuesta => {
+      numAdvice.innerHTML = `Advice #${respuesta['slip']['id']}`;
+      txtAdvice.innerHTML = `"${respuesta['slip']['advice']}"`;
+    });
 }
